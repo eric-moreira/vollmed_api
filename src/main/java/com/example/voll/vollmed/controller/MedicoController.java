afdba5e092b2b60d4fbd5eae43ab055eea4ab43b
eric-moreira <eric.ufjf@gmail.com>
2023-05-24 08:38:09 -0300

    added Listing on MedicosController

--- src/main/java/com/example/voll/vollmed/controller/MedicoController.java
+++ src/main/java/com/example/voll/vollmed/controller/MedicoController.java
@@ -1,16 +1,21 @@
 package com.example.voll.vollmed.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.voll.vollmed.models.Medico;
 import com.example.voll.vollmed.records.DadosCadastroMedico;
+import com.example.voll.vollmed.records.DadosListagemMedico;
 import com.example.voll.vollmed.repository.MedicoRepository;
 
 import jakarta.validation.Valid;
@@ -27,5 +32,10 @@ public class MedicoController {
     @Transactional
     public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados) {
         medicoRepository.save(new Medico(dados));
+    }
+
+    @GetMapping("/listar")
+    public List<DadosListagemMedico> listarMedicos() {
+        return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
---
+++ src/main/java/com/example/voll/vollmed/records/DadosListagemMedico.java
@@ -0,0 +1,17 @@
+package com.example.voll.vollmed.records;
+
+import com.example.voll.vollmed.models.Medico;
+
+public record DadosListagemMedico(
+    String nome,
+    String email,
+    String crm,
+    Especialidade especialidade
+
+) {
+
+    public DadosListagemMedico(Medico medico){
+        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+    
+}
